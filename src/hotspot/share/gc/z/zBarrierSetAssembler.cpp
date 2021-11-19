0d719d29f3b5d3961b30ed766db9e786f1ab83ae
Nils Eliasson <nils.eliasson@oracle.com>
2021-11-19 20:25:07 +0100

    Add verification of elided barriers

--- src/hotspot/share/gc/z/zBarrierSetAssembler.cpp
+++ src/hotspot/share/gc/z/zBarrierSetAssembler.cpp
@@ -34,6 +34,10 @@ Address ZBarrierSetAssemblerBase::mark_bad_mask_from_thread(Register thread) {
   return Address(thread, ZThreadLocalData::mark_bad_mask_offset());
 }
 
+Address ZBarrierSetAssemblerBase::store_bad_mask_from_thread(Register thread) {
+  return Address(thread, ZThreadLocalData::store_bad_mask_offset());
+}
+
 Address ZBarrierSetAssemblerBase::load_bad_mask_from_jni_env(Register env) {
   return Address(env, ZThreadLocalData::load_bad_mask_offset() - JavaThread::jni_environment_offset());
 }
