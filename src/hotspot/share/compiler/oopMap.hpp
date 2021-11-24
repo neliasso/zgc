ff2c60f7b7defcc9106713fab86045d53ff13364
Nils Eliasson <nils.eliasson@oracle.com>
2021-11-24 12:09:23 +0100

    Add indirect oopmap type

--- src/hotspot/share/compiler/oopMap.hpp
+++ src/hotspot/share/compiler/oopMap.hpp
@@ -57,7 +57,7 @@ private:
 
 public:
   // Constants
-  enum { type_bits                = 2,
+  enum { type_bits                = 3,
          register_bits            = BitsPerShort - type_bits };
 
   enum { type_shift               = 0,
@@ -73,28 +73,46 @@ public:
          narrowoop_value,
          callee_saved_value,
          derived_oop_value,
+         indirect_oop,
          unused_value = -1          // Only used as a sentinel value
   };
 
   // Constructors
   OopMapValue () { set_value(0); set_content_reg(VMRegImpl::Bad()); }
+
   OopMapValue (VMReg reg, oop_types t, VMReg reg2) {
     set_reg_type(reg, t);
     set_content_reg(reg2);
   }
 
- private:
-    void set_reg_type(VMReg p, oop_types t) {
+  OopMapValue (VMReg reg, short offset) {
+    set_reg_type(offset, indirect_oop);
+    set_content_reg(reg);
+  }
+
+  private:
+  void set_reg_type(VMReg p, oop_types t) {
     set_value((p->value() << register_shift) | t);
-    assert(reg() == p, "sanity check" );
-    assert(type() == t, "sanity check" );
+    assert(reg() == p, "sanity check");
+    assert(type() == t, "sanity check");
+  }
+
+  void set_reg_type(short s, oop_types t) {
+    assert(t == indirect_oop, "sanity");
+    set_value((s << register_shift) | t);
+    assert(reg_as_offset() == s, "sanity check");
+    assert(type() == t, "sanity check");
+  }
+
+  void set_content_offset(short offset) {
+    _content_reg = offset;
   }
 
   void set_content_reg(VMReg r) {
     if (is_callee_saved()) {
       // This can never be a stack location, so we don't need to transform it.
       assert(r->is_reg(), "Trying to callee save a stack location");
-    } else if (is_derived_oop()) {
+    } else if (is_derived_oop() || is_indirect_oop()) {
       assert (r->is_valid(), "must have a valid VMReg");
     } else {
       assert (!r->is_valid(), "valid VMReg not allowed");
@@ -106,14 +124,14 @@ public:
   // Archiving
   void write_on(CompressedWriteStream* stream) {
     stream->write_int(value());
-    if(is_callee_saved() || is_derived_oop()) {
+    if(is_callee_saved() || is_derived_oop() || is_indirect_oop()) {
       stream->write_int(content_reg()->value());
     }
   }
 
   void read_from(CompressedReadStream* stream) {
     set_value(stream->read_int());
-    if (is_callee_saved() || is_derived_oop()) {
+    if (is_callee_saved() || is_derived_oop() || is_indirect_oop()) {
       set_content_reg(VMRegImpl::as_VMReg(stream->read_int(), true));
     }
   }
@@ -123,15 +141,16 @@ public:
   bool is_narrowoop()         { return mask_bits(value(), type_mask_in_place) == narrowoop_value; }
   bool is_callee_saved()      { return mask_bits(value(), type_mask_in_place) == callee_saved_value; }
   bool is_derived_oop()       { return mask_bits(value(), type_mask_in_place) == derived_oop_value; }
+  bool is_indirect_oop()      { return mask_bits(value(), type_mask_in_place) == indirect_oop; }
 
-  VMReg reg() const { return VMRegImpl::as_VMReg(mask_bits(value(), register_mask_in_place) >> register_shift); }
+  VMReg reg() const           { return VMRegImpl::as_VMReg(mask_bits(value(), register_mask_in_place) >> register_shift); }
+  short reg_as_offset() const { return mask_bits(value(), register_mask_in_place) >> register_shift; }
   oop_types type() const      { return (oop_types)mask_bits(value(), type_mask_in_place); }
+  VMReg content_reg() const   { return VMRegImpl::as_VMReg(_content_reg, true); }
 
   static bool legal_vm_reg_name(VMReg p) {
     return (p->value()  == (p->value() & register_mask));
   }
-
-  VMReg content_reg() const       { return VMRegImpl::as_VMReg(_content_reg, true); }
 
   // Returns offset from sp.
   int stack_offset() {
@@ -142,7 +161,6 @@ public:
   void print_on(outputStream* st) const;
   void print() const;
 };
-
 
 class OopMap: public ResourceObj {
   friend class OopMapStream;
@@ -180,10 +198,11 @@ class OopMap: public ResourceObj {
   // Construction
   // frame_size units are stack-slots (4 bytes) NOT intptr_t; we can name odd
   // slots to hold 4-byte values like ints and floats in the LP64 build.
-  void set_oop  ( VMReg local);
+  void set_oop(VMReg local);
   void set_narrowoop(VMReg local);
-  void set_callee_saved( VMReg local, VMReg caller_machine_register );
-  void set_derived_oop ( VMReg local, VMReg derived_from_local_register );
+  void set_callee_saved(VMReg local, VMReg caller_machine_register);
+  void set_derived_oop(VMReg local, VMReg derived_from_local_register);
+  void set_indirect_oop(VMReg local, short offset);
 
   int heap_size() const;
   void copy_data_to(address addr) const;
