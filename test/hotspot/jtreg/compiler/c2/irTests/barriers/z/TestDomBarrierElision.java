ec04d2481cca5ff39543fee5fd7028fdd38f89d2
Nils Eliasson <nils.eliasson@oracle.com>
2022-02-24 13:36:23 +0100

    Add barrier test

---
+++ test/hotspot/jtreg/compiler/c2/irTests/barriers/z/TestDomBarrierElision.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package compiler.c2.irTests.barriers.z;
+
+import compiler.lib.ir_framework.*;
+
+/*
+ * @test
+ * @summary Test elision of dominating barriers in ZGC
+ * @library /test/lib /
+ * @run driver compiler.c2.irTests.barriers.z.TestDomBarrierElision
+ */
+
+public class TestDomBarrierElision {
+
+    class Payload {
+      Content c;
+      public Payload(Content c) {
+          this.c = c;
+      }
+    }
+
+    class Content {
+        int id;
+        public Content(int id) {
+            this.id = id;
+        }
+    }
+
+    Payload p = new Payload(new Content(5));
+    Content c1 = new Content(45);
+    Content c2 = new Content(15);
+
+    public static void main(String[] args) {
+        TestFramework framework = new TestFramework();
+        Scenario zgc = new Scenario(0, "-XX:+UseZGC", "-XX:CompileCommand=dontinline,*::blackhole");
+        framework.addScenarios(zgc).start();
+    }
+
+    static void blackhole(Content t) {
+        // nothing
+    }
+
+    static void blackhole(Payload p) {
+        // nothing
+    }
+
+    @Test
+    @IR(counts = { IRNode.LOAD_P,  "1" })
+    //@IR(counts = { IRNode.LOAD_B,  "1" })
+    private static Content testBasicLoad(Payload p) {
+        return p.c;
+    }
+
+    @Test
+    @IR(counts = { IRNode.STORE_P,  "1" })
+    private static Content testBasicStore(Payload p) {
+        return p.c = null;
+    }
+
+    @Test
+    @IR(counts = { IRNode.LOAD_P,  "2" })
+    private static Content testBasicLoadDom(Payload p) {
+        Content t = p.c;
+        blackhole(t);
+        return p.c;
+    }
+
+    @Test
+    @IR(counts = { IRNode.STORE_P,  "2" })
+    private static void testBasicStoreDom(Payload p, Content t1, Content t2) {
+        p.c = t1;
+        blackhole(p);
+        p.c = t2;
+    }
+
+    @Test
+    @IR(counts = { IRNode.STORE_P,  "1" })
+    @IR(counts = { IRNode.LOAD_P,   "1" })
+    private static Content testBasicStoreLoadDom(Payload p, Content t1) {
+        p.c = t1;
+        blackhole(p);
+        return p.c;
+    }
+
+    @Test
+    @IR(counts = { IRNode.STORE_P,  "1" })
+    @IR(counts = { IRNode.LOAD_P,   "1" })
+    private static void testBasicLoadStoreDom(Payload p, Content t1) {
+        Content t = p.c;
+        blackhole(t);
+        p.c = t1;
+    }
+
+    @Run(test = {"testBasicLoad",
+                "testBasicStore",
+                "testBasicLoadDom",
+                "testBasicStoreDom",
+                "testBasicStoreLoadDom",
+                "testBasicLoadStoreDom"})
+    private void testBasic_runner() {
+        testBasicLoad(p);
+        testBasicStore(p);
+        testBasicLoadDom(p);
+        testBasicStoreDom(p, c1, c2);
+        testBasicStoreLoadDom(p, c1);
+        testBasicLoadStoreDom(p, c1);
+    }
+}
